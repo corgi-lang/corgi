e62b602d35ac82c536e9a033b517b10d6ecf75b9
Nektro <nektromc@gmail.com>
2017-10-10 01:21:51 -0400

    do escape for quote escape sequence

--- corgi.js
+++ corgi.js
@@ -141,7 +141,7 @@
                     return (`${a}="${b}"`);
                 }
                 case 'String': {
-                    return (value.substring(1, value.length - 1));
+                    return (value.substring(1, value.length - 1)).split(`\\"`).join(`"`);
                 }
                 case 'ElementIC': {
                     [a,b] = value.map(this.compileExpr.bind(this));
